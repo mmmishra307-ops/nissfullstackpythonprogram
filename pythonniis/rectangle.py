1821624c8e88b3c2181c771557ceed40ff5b501b
Manisha Manaswita Mishra <m.mmishra307@gmail.com>
2026-02-20 12:33:31 +0530

    test

---
+++ pythonniis/rectangle.py
@@ -0,0 +1,10 @@
+print("enter rectangle length")
+L=int(input())
+print("enter renctangle breadth")
+B=int(input())
+ar=L*B
+pr=2*(L+B)
+print("length=",L)
+print("bredth=",B)
+print("area=",ar)
+print("perimeter=",pr)
