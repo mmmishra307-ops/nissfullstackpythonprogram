5905043a5f18f5a145768d84ccd1ed997c7f9d6a
Manisha Manaswita Mishra <m.mmishra307@gmail.com>
2026-03-17 21:51:23 +0530

    test

---
+++ pythonniis/string.py/predefined.py
@@ -0,0 +1,38 @@
+s="ram is a Good boy"
+s=s.title()
+print(s)
+
+
+s="ram is a Good boy"
+s=s.capitalize()
+print(s)
+
+
+s="ram is a Good boy"
+s=s.upper()
+print(s)
+
+
+s="rAM is a Good boy"
+s=s.lower()
+print(s)
+
+
+s="rAM is a Good boy"
+s=s.casefold()
+print(s)
+
+
+s="rAM is a Good boy"
+s=(s.swapcase())
+print(s)
+
+
+s="ram is a Good boy"
+s=s.upper()
+print(s)
+
+
+s="rAM is a Good boy"
+s=s.lower()
+print(s)
