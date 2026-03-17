4343ad1d44cac3d4e25e2701a371bd99e8ad57c4
Manisha Manaswita Mishra <m.mmishra307@gmail.com>
2026-03-17 21:38:36 +0530

    test

---
+++ pythonniis/string.py/operator.py
@@ -0,0 +1,32 @@
+s='ram'
+print(s*3)
+s1="das"
+print(s+s1)
+print('a'in s1)
+print('x'in s1)
+s2="abc"
+s3="Abc"
+print(s2==s3)
+print(s2>=s3)
+print(s2<=s3)
+print(s2!=s3)
+
+
+
+#wap count no char in string
+s="welcome"
+print(len(s))
+print(len("ok"))
+
+
+#wap count no char in string without len() function
+s="welcome"
+c=0
+for i in s:
+	c=c+1
+print("no of char=",c)
+
+
+
+
+
